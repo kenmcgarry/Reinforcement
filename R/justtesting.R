7b9f87a530e83a0e43e192f170860784518c9b33
Ken McGarry <ken.mcgarry@sunderland.ac.uk>
2019-06-28 07:33:45 +0100

    Add files via upload

--- R/justtesting.R
+++ R/justtesting.R
@@ -1,12 +1,10 @@
 
-IGnames <- c("sex","sibsp","pclass","age","parch")
-scum[1,1]
-test <- str_extract_all(scum[3,1], regex(IGnames)) %>% unlist()
+
 
 length(test)
-fucked<-"absent"
+doom <- "absent"
 
-if(grep(rules_kyphosis[1,1],fucked)==1){}
+if(grep(rules_kyphosis[1,1],doom)==1){}
 
 
 
